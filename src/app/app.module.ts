af9d70a8c8d6fd37d110af46a31769e2e949044e
Carina Soca <cari.soca.13@gmail.com>
2017-08-23 00:12:38 -0300

    Maquetado de componentes basicos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { DocumentListComponent } from './side-bar/document-list/document-list.component';
+import { CodeListComponent } from './side-bar/code-list/code-list.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { DocumentComponent } from './content/document/document.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    DocumentListComponent,
+    CodeListComponent,
+    ContentComponent,
+    FooterComponent,
+    DocumentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/side-bar/side-bar.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.css']
+})
+export class SideBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  sidenavCollapse() {
+    console.log('asd');
+    if (document.getElementById('sidenavCollapseBtn').className === 'opened') {
+        document.getElementById('sidenavCollapseBtn').className = 'closed';
+        document.getElementById('sidenavCollapseBtn').firstElementChild.className = 'glyphicon glyphicon-chevron-right';
+        document.getElementById('main').style.marginLeft = '0%';
+        document.getElementById('mysidenav').style.display = 'none';
+    } else {
+        document.getElementById('sidenavCollapseBtn').className = 'opened';
+        document.getElementById('sidenavCollapseBtn').firstElementChild.className = 'glyphicon glyphicon-chevron-left';
+        document.getElementById('mysidenav').style.width = '20%';
+        document.getElementById('mysidenav').style.display = 'block';
+    }
+}
+
+}
